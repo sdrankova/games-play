d1b4dec85305fa2626345ec3fa1bbdbf87d781bc
sdrankova <sanny9676@gmail.com>
2023-11-14 14:08:56 +0200

    create game service

--- client/src/components/game-create/GameCreate.jsx
+++ client/src/components/game-create/GameCreate.jsx
@@ -1,23 +1,29 @@
 export default function GameCreate() {
+    const createGameSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const gameData = Object.fromEntries(new FormData(e.currentTarget));
+        console.log(gameData)
+    };
+
     return (
         <section id="create-page" className="auth">
-            <form id="create">
+            <form id="create" onSubmit={createGameSubmitHandler}>
                 <div className="container">
-
                     <h1>Create Game</h1>
-                    <label htmlfor="leg-title">Legendary title:</label>
+                    <label htmlFor="leg-title">Legendary title:</label>
                     <input type="text" id="title" name="title" placeholder="Enter game title..." />
 
-                    <label htmlfor="category">Category:</label>
+                    <label htmlFor="category">Category:</label>
                     <input type="text" id="category" name="category" placeholder="Enter game category..." />
 
-                    <label htmlfor="levels">MaxLevel:</label>
+                    <label htmlFor="levels">MaxLevel:</label>
                     <input type="number" id="maxLevel" name="maxLevel" min="1" placeholder="1" />
 
-                    <label htmlfor="game-img">Image:</label>
+                    <label htmlFor="game-img">Image:</label>
                     <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
 
-                    <label htmlfor="summary">Summary:</label>
+                    <label htmlFor="summary">Summary:</label>
                     <textarea name="summary" id="summary"></textarea>
                     <input className="btn submit" type="submit" value="Create Game" />
                 </div>
